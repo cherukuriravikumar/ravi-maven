745c65e8e72bd5e72fe82c8b9a9e7bbb31ccccd5
Ravi kumar <cherukuriravikumar930@gmail.com>
2021-08-20 16:39:06 +0530

    Initial checkin

---
+++ src/main/java/com/ravi/devops/ravi_maven/App.java
@@ -0,0 +1,13 @@
+package com.ravi.devops.ravi_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from ravi" );
+    }
+}
---
+++ src/test/java/com/ravi/devops/ravi_maven/AppTest.java
@@ -0,0 +1,20 @@
+package com.ravi.devops.ravi_maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
